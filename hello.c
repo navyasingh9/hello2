da57c3f4655847dcb8b4a5600c3c13dbb013800d
sifatsyed <42530377+sifatsyed@users.noreply.github.com>
2019-08-29 12:34:40 -0500

    New changes made to hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Austin and Navya
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Navya and my major is Computer Science, and I'm austin and my major is Software Engineering!\n");
+  printf("I'm Sifat Syed and my major is computer science\n");
+  return 0;
+}
